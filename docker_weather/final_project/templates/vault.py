d451f1c9374fc6912985ac447df6740c7d48746b
guy.romano <guyromano10@gmail.com>
2024-04-04 18:36:42 +0300

    testing vault

--- docker_weather/final_project/templates/vault.py
+++ docker_weather/final_project/templates/vault.py
@@ -6,8 +6,8 @@ client.auth.approle.login(
     role_id="91410109-d361-420c-5de6-d4fc86998706",
     secret_id="f0c6a0b5-dabf-39e3-0ae2-59036a010dc2",)
 
-read_response = client.secrets.kv.read_secret_version(mount_point="kv", path='kv/weatherapp/text')
+read_response = client.secrets.kv.read_secret_version(mount_point="kv", path='weatherapp/text')
 
-password = read_response['data']['data']['password']
+password = read_response['data']['data']['Assignment']
 print(password)
 
